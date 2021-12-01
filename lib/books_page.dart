ba9850d5dd541c7bdb1c8f7dc7ed8b48ad6f494f
sadoyol <sadoyol@hotmail.com>
2021-12-01 20:51:56 +0300

    firestore

--- lib/books_page.dart
+++ lib/books_page.dart
@@ -3,6 +3,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
 import 'comment_page.dart';
 import 'login_page.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
+  // FirebaseFirestore firestore = FirebaseFirestore.instance;
+  WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp();
+
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
